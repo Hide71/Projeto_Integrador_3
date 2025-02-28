8e305a311eee8240e3d9cb4fe0f93347d21fb4ab
Bruno Rodrigues Marciano <bruno-9237@hotmail.com>
2025-02-28 20:15:42 -0300

    estrutura de testes

---
+++ Controle_Pessoal.Tests/Endpoints/Users/CreateUserTests.cs
@@ -0,0 +1,51 @@
+﻿using Controle_Pessoal.Tests.Fixtures;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Controle_Pessoal.Tests.Endpoints.Users
+{
+    [Trait("Api", "Users - Create")]
+    public class CreateUserTests : ApiTest
+    {
+        public CreateUserTests(ApiFixture monnyApiFixture) : base(monnyApiFixture)
+        {
+        }
+
+        [Fact]
+        public async Task DeveCriarUsuarioERetornarCreated_QuandoRequisicaoForValida()
+        {
+            // Arrange
+            var request= new
+            {
+                username = Faker.Person.UserName,
+                email = Faker.Person.Email,
+                url = Faker.Person.Website
+            };
+
+            // Act
+            var httpResponse = await ApiClient.PostAsJsonAsync("/v1/user", request, TestContext.Current.CancellationToken);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+            Assert.Equal("v1/users/1", httpResponse.Headers.Location.ToString());
+        }
+
+        [Fact]
+        public async Task DeveRetornarBadRequest_QuandoRequisicaoForValida()
+        {
+            // Arrange
+            var request = new
+            {
+                username = "",
+                email = "",
+                url = ""
+            };
+
+            // Act
+            var httpResponse = await ApiClient.PostAsJsonAsync("/v1/user", request, TestContext.Current.CancellationToken);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, httpResponse.StatusCode);
+        }
+    }
+}
